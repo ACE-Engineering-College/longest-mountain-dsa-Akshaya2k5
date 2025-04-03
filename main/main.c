f4085b725e2cdfed471b4103ac11934abf61aca5
Akshaya2k5 <166425815+Akshaya2k5@users.noreply.github.com>
2025-04-03 11:13:04 +0530

    Update main.c

--- main/main.c
+++ main/main.c
@@ -2,5 +2,30 @@
 // Function to find the longest mountain in an array..
 int longestMountain(int arr[], int n)
 {
-    // code for Longest Mountain
+    if (arrSize < 3) 
+        return 0;  
+    int ans = 0;
+    int i = 1;
+    int upper, lower;
+
+    while (i < arrSize) 
+    {    
+        upper = 0;
+        lower = 0;                  
+        while (i < arrSize && arr[i - 1] == arr[i])    
+            i++;                            
+        while (i < arrSize && arr[i - 1] < arr[i]) {  
+            upper++;
+            i++;
+        }
+        while (i < arrSize && arr[i - 1] > arr[i]) {
+            lower++;
+            i++;    
+        } 
+        if (upper > 0 && lower > 0) { 
+            ans = fmax(ans, lower + upper + 1);
+        }
+    }
+    
+    return ans;
 }
